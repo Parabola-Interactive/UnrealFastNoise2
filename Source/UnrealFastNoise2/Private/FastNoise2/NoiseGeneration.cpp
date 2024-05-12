2e2b7f7cb6930cdb05ccbc55e3e5f7093ee06e3e
przemyslawlis <przemyslawlis@parabola-interactive.com>
2024-05-12 13:49:06 +0200

    result of noise generation in TArray<float>

--- Source/UnrealFastNoise2/Private/FastNoise2/NoiseGeneration.cpp
+++ Source/UnrealFastNoise2/Private/FastNoise2/NoiseGeneration.cpp
@@ -4,16 +4,22 @@
 
 int UNoiseGeneration::GetRandomSeed()
 {
-    return FMath::RandRange(INT_MIN, INT_MAX);
+	return FMath::RandRange(INT_MIN, INT_MAX);
 }
 
-void UNoiseGeneration::GenPerlin2D(float* Out, const int XSize, const int YSize, const float Frequency)
+void UNoiseGeneration::GenPerlin2D(TArray<float>& Out, const int XSize, const int YSize, const float Frequency)
 {
-    return GenPerlin2D(Out, XSize, YSize, Frequency, GetRandomSeed());
+	return GenPerlin2D(Out, XSize, YSize, Frequency, GetRandomSeed());
 }
 
-void UNoiseGeneration::GenPerlin2D(float* Out, const int XSize, const int YSize, const float Frequency, const int Seed)
+void UNoiseGeneration::GenPerlin2D(TArray<float>& Out, const int XSize, const int YSize, const float Frequency, const int Seed)
 {
-    const FastNoise::SmartNode<FastNoise::Perlin> Noise = FastNoise::New<FastNoise::Perlin>();
-    Noise->GenTileable2D(Out, XSize, YSize, Frequency, Seed);
+	const FastNoise::SmartNode<FastNoise::Perlin> Noise = FastNoise::New<FastNoise::Perlin>();
+
+	const int ElemCount = XSize * YSize;
+	float* Result = new float[ElemCount];
+	Noise->GenTileable2D(Result, XSize, YSize, Frequency, Seed);
+
+	Out.Reserve(ElemCount);
+	for (int i = 0; i < ElemCount; i++) Out.Add(Result[i]);
 }
--- Source/UnrealFastNoise2/Public/FastNoise2/NoiseGeneration.h
+++ Source/UnrealFastNoise2/Public/FastNoise2/NoiseGeneration.h
@@ -11,6 +11,6 @@ class UNREALFASTNOISE2_API UNoiseGeneration : public UObject
 	static int GetRandomSeed();
 	
 public:
-	static void GenPerlin2D(float* Out, int XSize, int YSize, float Frequency);
-	static void GenPerlin2D(float* Out, int XSize, int YSize, float Frequency, int Seed);
+	static void GenPerlin2D(TArray<float>& Out, int XSize, int YSize, float Frequency);
+	static void GenPerlin2D(TArray<float>& Out, int XSize, int YSize, float Frequency, int Seed);
 };
