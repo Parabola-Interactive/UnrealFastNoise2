7e978a953828a48555876ce85137fc6f9f4d1826
Przemyslaw Lis <przemyslawlis@parabola-interactive.com>
2025-05-13 09:50:19 +0200

    GenPerlin2D now uses uniform grid 2d generation added xstart and ystart as arguments get random seed method now uses int min and int max for random range

--- Source/UnrealFastNoise2/Private/FastNoise2/NoiseGeneration.cpp
+++ Source/UnrealFastNoise2/Private/FastNoise2/NoiseGeneration.cpp
@@ -4,22 +4,24 @@
 
 int UNoiseGeneration::GetRandomSeed()
 {
-	return FMath::RandRange(-1000000000, 1000000000);
+	constexpr int64 Min = INT_MIN;
+	constexpr int64 Max = INT_MAX;
+	return FMath::RandRange(Min, Max);
 }
 
-void UNoiseGeneration::GenPerlin2D(TArray<float>& Out, const int XSize, const int YSize, const float Frequency, const float Scale)
+void UNoiseGeneration::GenPerlin2D(TArray<float>& Out, const int XStart, const int YStart, const int XSize, const int YSize, const float Frequency, const float Scale)
 {
 	const int Seed = GetRandomSeed();
-	return GenPerlin2D(Out, XSize, YSize, Frequency, Scale, Seed);
+	return GenPerlin2D(Out, XStart, YStart, XSize, YSize, Frequency, Scale, Seed);
 }
 
-void UNoiseGeneration::GenPerlin2D(TArray<float>& Out, const int XSize, const int YSize, const float Frequency, const float Scale, const int Seed)
+void UNoiseGeneration::GenPerlin2D(TArray<float>& Out, const int XStart, const int YStart, const int XSize, const int YSize, const float Frequency, const float Scale, const int Seed)
 {
 	const FastNoise::SmartNode<FastNoise::Perlin> Noise = FastNoise::New<FastNoise::Perlin>();
 
 	const int ElemCount = XSize * YSize;
 	float* Result = new float[ElemCount];
-	Noise->GenTileable2D(Result, XSize, YSize, Frequency, Seed);
+	Noise->GenUniformGrid2D(Result, XStart, YStart, XSize, YSize, Frequency, Seed);
 
 	Out.Reserve(ElemCount);
 	for (int i = 0; i < ElemCount; i++) Out.Add(Result[i] * Scale);
--- Source/UnrealFastNoise2/Public/FastNoise2/NoiseGeneration.h
+++ Source/UnrealFastNoise2/Public/FastNoise2/NoiseGeneration.h
@@ -7,10 +7,9 @@ UCLASS(Abstract)
 class UNREALFASTNOISE2_API UNoiseGeneration : public UObject
 {
 	GENERATED_BODY()
-
-	static int GetRandomSeed();
 	
 public:
-	static void GenPerlin2D(TArray<float>& Out, const int XSize, const int YSize, const float Frequency, const float Scale);
-	static void GenPerlin2D(TArray<float>& Out, const int XSize, const int YSize, const float Frequency, const float Scale, const int Seed);
+	static void GenPerlin2D(TArray<float>& Out, const int XStart, const int YStart, const int XSize, const int YSize, const float Frequency, const float Scale);
+	static void GenPerlin2D(TArray<float>& Out, const int XStart, const int YStart, const int XSize, const int YSize, const float Frequency, const float Scale, const int Seed);
+	static int GetRandomSeed();
 };
